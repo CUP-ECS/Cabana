2a292c250ebc3b83705a3ca41ec959421bb00833
Carson Woods <cwoods289@gmail.com>
2023-01-19 18:48:45 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+# TODO: YOU, THE USER, SHOULD CHANGE THESE TO YOUR DESIRED PATHS
+export KOKKOS_INSTALL_DIR=$(pwd)/kokkos/build/install
+export CABANA_INSTALL_DIR=$(pwd)/Cabana/build/install
+
+cd ./Cabana
+mkdir - build/install
+
+cd build
+cmake \
+    -D CMAKE_BUILD_TYPE="Debug" \
+    -D CMAKE_PREFIX_PATH=$KOKKOS_INSTALL_DIR \
+    -D CMAKE_INSTALL_PREFIX=$CABANA_INSTALL_DIR \
+    -D Cabana_REQUIRE_OPENMP=ON \
+    -D Cabana_ENABLE_EXAMPLES=ON \
+    -D Cabana_ENABLE_TESTING=ON \
+    -D Cabana_ENABLE_PERFORMANCE_TESTING=OFF \
+    -D Cabana_ENABLE_CAJITA=ON \
+    .. ;
+
+make install
